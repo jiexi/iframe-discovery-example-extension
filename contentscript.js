a73501f09fae943dad4ecd7956e18d55a3c1c456
Jiexi Luan <jiexiluan@gmail.com>
2024-03-20 10:51:04 -0700

    Initial commit

---
+++ contentscript.js
@@ -0,0 +1,17 @@
+console.log('iframe contentscript: injected')
+
+function announceExtension() {
+  var data = { extensionId: 'some-extension-id' }
+  var event = { type: 'announceExtension', data }
+  window.top.postMessage(event, '*')
+  console.log('iframe contentscript: dispatched announceExtension')
+}
+
+announceExtension()
+
+window.addEventListener('message', event => {
+  if (event.data.type === 'requestExtensions') {
+    console.log('iframe contentscript: received requestExtensions', event.data)
+    announceExtension()
+  }
+});
